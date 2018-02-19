9e355adbfff8ca9c33460b3005e836758b67f276
kungminlin <33560439+kungminlin@users.noreply.github.com>
2018-02-18 22:07:52 -0800

    first commit

---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<header id="header">
+	<h1 id="logo"><a href="index.php">LAHS <span style="color:#66bb6a">GREEN TEAM</span></a></h1>
+	<nav id="nav">
+		<ul>
+			<li class="current"><a href="../index.php">HOME</a></li>
+			<li class="submenu">
+				<a href="#">MENU</a>
+				<ul>
+					<li><a href="about.php">ABOUT</a></li>
+					<li><a href="events.php">EVENTS AND CONFERENCES</a></li>
+					<!-- <li><a href="posts.php">POSTS</a></li> -->
+					<li><a href="contact.php">CONTACT</a></li>
+<!-- 									<li class="submenu">
+						<a href="#">Submenu</a>
+						<ul>
+							<li><a href="#">Dolore Sed</a></li>
+							<li><a href="#">Consequat</a></li>
+							<li><a href="#">Lorem Magna</a></li>
+							<li><a href="#">Sed Magna</a></li>
+							<li><a href="#">Ipsum Nisl</a></li>
+						</ul>
+					</li> -->
+				</ul>
+			</li>
+			<!-- <li><a href="#" class="button special">Sign Up</a></li> -->
+		</ul>
+	</nav>
+</header>
